58df3f52f403cb653b24035b8a541d4a44323287
Benita Daniel <benitadaniel.dev@gmail.com>
2023-04-26 02:44:34 +0100

    pricing page completed

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".next",
-      prevEl: ".prev",
-    },
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 20,
+//     loop: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".next",
+//       prevEl: ".prev",
+//     },
+//   });
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  const icon = question.querySelector("i");
+  const answer = question.nextElementSibling;
+  question.addEventListener("click", () => {
+    question.classList.toggle("active");
+    answer.classList.toggle("active");
+    icon.classList.toggle("fa-chevron-down");
+    icon.classList.toggle("fa-chevron-up");
   });
+});
+
+  
